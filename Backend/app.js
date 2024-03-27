a51f529253fbd51a4351c1b09bb545fa8a6ac2ae
Pevinya <90928087+Pevinya@users.noreply.github.com>
2024-03-27 23:13:42 +0530

    register added

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json()) //strcturing the request body
 
 require("dotenv").config();
+app.use(cors());
 const dbConfig=require("./config/dbConfig");
 
 const PORT = process.env.PORT || 5000;
