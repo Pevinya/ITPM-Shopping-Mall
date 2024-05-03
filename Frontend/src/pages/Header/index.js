5d123ae1f05697b9d8ebe98b8fbec5f74e1a86a0
it21509186 <99590133+it21509186@users.noreply.github.com>
2024-05-03 22:53:38 +0530

    changed logo

--- Frontend/src/pages/Header/index.js
+++ Frontend/src/pages/Header/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Layout, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import logo from "../../images/home/pinnacle_logo.png";
+import logo from "../../images/home/Pinna-removebg-preview.png";
 
 const { Header } = Layout;
 
@@ -15,11 +15,11 @@ const AppHeader = () => {
   return (
     <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#EAE2F8', padding: '0 10px' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        {/* Make the logo smaller */}
-        <img src={logo} alt="Pinnacle Arcade Logo" style={{ width: '70px', marginRight: '20px' }} />
+        {/* Make the logo larger */}
+        <img src={logo} alt="Pinnacle Arcade Logo" style={{ width: '100px', marginRight: '20px' }} />
         {/* Add CSS for increased spacing between menu items */}
         <nav>
-          <ul style={{ display: 'flex', listStyleType: 'none', margin: 0, padding: 250 }}>
+          <ul style={{ display: 'flex', listStyleType: 'none', margin: 0, padding: 230 }}>
             <li style={{ marginRight: '80px' }}>
               <a href="#home" onClick={() => handleClick('home')} style={{ color: activeMenu === 'home' ? '#A875FF' : '#000', fontWeight: 'bold', textDecoration: 'none' }}>Home</a>
             </li>
