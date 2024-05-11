8b171431516fa6321f4dea928484b14d91a00a9b
Manilka Navod <manilka.projects@gmail.com>
2024-05-11 11:31:18 +0530

    user profile functionality added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,38 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Users from './pages/Profile';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Users from "./pages/Profile";
 
-import CreateFeedback from './pages/CreateFeedback';
-import DisplayFeedback from './pages/DisplayFeedback';
-import FeedbacksByUser from './pages/FeedbacksByUser';
+import CreateFeedback from "./pages/CreateFeedback";
+import DisplayFeedback from "./pages/DisplayFeedback";
+import FeedbacksByUser from "./pages/FeedbacksByUser";
 
-import Packages from './pages/Packages';
-import Shopping from './pages/shoppingPage';
-import Clothes from './pages/Levels/clothes';
-import Phones from './pages/Levels/phones';
-import Cosmetics from './pages/Levels/cosmetics';
+import Packages from "./pages/Packages";
+import Shopping from "./pages/shoppingPage";
+import Clothes from "./pages/Levels/clothes";
+import Phones from "./pages/Levels/phones";
+import Cosmetics from "./pages/Levels/cosmetics";
 
+import "./stylesheets/allignment.css";
+import "./stylesheets/theme.css";
+import "./stylesheets/sizes.css";
+import "./stylesheets/customcomponenets.css";
+import "./stylesheets/formelements.css";
 
-import './stylesheets/allignment.css';
-import './stylesheets/theme.css';
-import './stylesheets/sizes.css';
-import './stylesheets/customcomponenets.css';
-import './stylesheets/formelements.css';
-
-import ProtectedRoute from './Components/ProtectedRoute';
-import AddZara from './pages/addProducts/zara';
-import ShowProducts from './pages/showProduct';
-import StoreDirectory from './pages/Levels/groceries';
-import AdminAdd from './pages/adminAdd/clothes';
-import AppHeader from './pages/Header';
-import AppFooter from './pages/Footer';
-import UserTable from './pages/Profile';
-import UserDetailsForm from './pages/Profile/viewprofile';
-import ShoppingListView from './pages/shoppinglist/listview';
-
+import ProtectedRoute from "./Components/ProtectedRoute";
+import AddZara from "./pages/addProducts/zara";
+import ShowProducts from "./pages/showProduct";
+import StoreDirectory from "./pages/Levels/Groceries";
+import AdminAdd from "./pages/adminAdd/clothes";
+import AppHeader from "./pages/Header";
+import AppFooter from "./pages/Footer";
+import UserTable from "./pages/Profile";
+import UserDetailsForm from "./pages/Profile/viewprofile";
+import ShoppingListView from "./pages/shoppinglist/listview";
 
 function App() {
   return (
@@ -40,68 +38,41 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/*Pevinya*/}
-          <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/UserDetailsForm' element={<UserDetailsForm/>} />
-          
-          <Route path='/ShoppingListView' element={<ShoppingListView/>} />
-        
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/UserDetailsForm" element={<UserDetailsForm />} />
 
-
-
-
-
-
-
+          <Route path="/ShoppingListView" element={<ShoppingListView />} />
 
           {/*Lakitha */}
-          <Route path='/feedback' element={<CreateFeedback />} />
-          <Route path='/displayFeedbacks' element={<DisplayFeedback />} /> 
-          <Route path='/feedbacksByUser' element={<FeedbacksByUser />} /> 
+          <Route path="/feedback" element={<CreateFeedback />} />
+          <Route path="/displayFeedbacks" element={<DisplayFeedback />} />
+          <Route path="/feedbacksByUser" element={<FeedbacksByUser />} />
 
+          {/*Nishedi */}
+          <Route path="/package" element={<Packages />} />
+          <Route path="/level3" element={<Phones />} />
+          <Route path="/level4" element={<Cosmetics />} />
 
+          {/*Lakshima */}
+          {/* <Route path='/add-product/ZaraLogo' element={<AddZara />} /> */}
+          <Route path="/shopping" element={<Shopping />} />
+          <Route path="/header" element={<AppHeader />} />
+          <Route path="/footer" element={<AppFooter />} />
+          <Route path="/level1" element={<StoreDirectory />} />
+          <Route path="/level2" element={<Clothes />} />
+          <Route path="/showProducts" element={<ShowProducts />} />
+          <Route path="/adminAdd" element={<AdminAdd />} />
 
-
-
-
-
-
-
-           {/*Nishedi */}
-           <Route path='/package' element={<Packages />} />
-           <Route path='/level3' element={<Phones/>}/>
-           <Route path='/level4' element={<Cosmetics/>}/>
-
-
-          
-
-
-
-
-            {/*Lakshima */}
-            {/* <Route path='/add-product/ZaraLogo' element={<AddZara />} /> */}
-            <Route path='/shopping' element={<Shopping/>}/>
-            <Route path='/header' element={<AppHeader/>} />
-            <Route path='/footer' element={<AppFooter/>} />
-            <Route path='/level1' element={<StoreDirectory/>}/>
-            <Route path='/level2' element={<Clothes/>}/>
-            <Route path='/showProducts' element={<ShowProducts/>}/>
-            <Route path='/adminAdd' element={<AdminAdd/>}/>
-            
-        <Route exact path="/add-product/:name" element={<AddZara />} />
-
-
-
-
-                    
-
-
-
-
-
-    
-          
+          <Route exact path="/add-product/:name" element={<AddZara />} />
         </Routes>
       </BrowserRouter>
     </div>
--- Frontend/src/pages/Profile/viewprofile.js
+++ Frontend/src/pages/Profile/viewprofile.js
@@ -1,5 +1,16 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Card, Button, Row, Col, Typography, Modal, Form, Input } from 'antd';
+import React, { useState, useEffect } from "react";
+import {
+  Layout,
+  Menu,
+  Card,
+  Button,
+  Row,
+  Col,
+  Typography,
+  Modal,
+  Form,
+  Input,
+} from "antd";
 import {
   PieChartOutlined,
   DesktopOutlined,
@@ -9,8 +20,13 @@ import {
   EditOutlined,
   DeleteOutlined,
   LogoutOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 //import 'antd/dist/antd.css';
+import {
+  GetLoggedInUserDetails,
+  editUser,
+  deleteUser,
+} from "../../apicalls/users";
 
 const { Header, Content, Sider } = Layout;
 const { Text } = Typography;
@@ -18,18 +34,41 @@ const { Text } = Typography;
 const AppLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [userData, setUserData] = useState({
-    username: "JohnDoe",
-    email: "john.doe@example.com"
-  });
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await GetLoggedInUserDetails();
+        if (response.success) {
+          setUserData(response.data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+
+    getUserData();
+  }, []);
 
   const showEditModal = () => {
     setIsModalVisible(true);
   };
 
-  const handleEditUser = (values) => {
-    console.log('Updated Data', values);
-    setUserData(values);
+  const handleEditUser = async (values) => {
+    console.log("Updated Data", values);
+
+    try {
+      const response = await editUser(values);
+      if (response.success) {
+        console.log("User data updated successfully:", response.data);
+        setUserData(values);
+      }
+    } catch (err) {
+      console.error("Failed to update user data:", err);
+    }
+
     setIsModalVisible(false);
   };
 
@@ -37,16 +76,44 @@ const AppLayout = () => {
     setIsModalVisible(false);
   };
 
-  const handleDeleteUser = () => {
-    console.log('User deleted');
-    // Implement delete functionality
+  const showDeletModal = () => {
+    setIsDeleteModalVisible(true);
+  };
+
+  const handleDeletCancel = () => {
+    setIsDeleteModalVisible(false);
+  };
+
+  const handleDeleteUser = async (values) => {
+    console.log("User deleted");
+    const { password } = values;
+    const { email } = userData;
+    console.log(password);
+    try {
+      const response = await deleteUser({ email, password });
+      if (response.success) {
+        console.log("User data deleted successfully:");
+        // remove token from localstorage
+        localStorage.removeItem("token");
+        // redirect to login page
+        window.location.href = "/login";
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-        <div style={{ height: '32px', background: 'rgba(255, 255, 255, 0.2)', margin: '16px' }} />
-        <Menu theme="dark" defaultSelectedKeys={['4']} mode="inline">
+        <div
+          style={{
+            height: "32px",
+            background: "rgba(255, 255, 255, 0.2)",
+            margin: "16px",
+          }}
+        />
+        <Menu theme="dark" defaultSelectedKeys={["4"]} mode="inline">
           <Menu.Item key="1" icon={<PieChartOutlined />}>
             Dashboard
           </Menu.Item>
@@ -71,17 +138,31 @@ const AppLayout = () => {
         </Menu>
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: '#fff' }} />
-        <Content style={{ margin: '0 16px' }}>
+        <Header style={{ padding: 0, background: "#fff" }} />
+        <Content style={{ margin: "0 16px" }}>
           <Row gutter={16}>
             <Col span={24}>
               <Card title="User Details" bordered={false}>
-                <p><Text strong>Username:</Text> {userData.username}</p>
-                <p><Text strong>Email:</Text> {userData.email}</p>
-                <Button type="primary" icon={<EditOutlined />} onClick={showEditModal} style={{ marginRight: 8 }}>
+                <p>
+                  <Text strong>Username:</Text> {userData?.name}
+                </p>
+                <p>
+                  <Text strong>Email:</Text> {userData?.email}
+                </p>
+                <Button
+                  type="primary"
+                  icon={<EditOutlined />}
+                  onClick={showEditModal}
+                  style={{ marginRight: 8 }}
+                >
                   Edit
                 </Button>
-                <Button type="danger" icon={<DeleteOutlined />} onClick={handleDeleteUser}>
+                <Button
+                  type="primary"
+                  icon={<DeleteOutlined />}
+                  onClick={showDeletModal}
+                  danger
+                >
                   Delete
                 </Button>
               </Card>
@@ -89,29 +170,57 @@ const AppLayout = () => {
           </Row>
         </Content>
       </Layout>
-      <Modal title="Edit User Details" visible={isModalVisible} onCancel={handleCancel} footer={null}>
+      <Modal
+        title="Edit User Details"
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
         <Form
           layout="vertical"
           initialValues={userData}
           onFinish={handleEditUser}
         >
           <Form.Item
-            name="username"
+            name="name"
             label="Username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+            rules={[{ required: true, message: "Please input your username!" }]}
           >
             <Input />
           </Form.Item>
           <Form.Item
             name="email"
             label="Email"
-            rules={[{ required: true, message: 'Please input your email!' }]}
+            rules={[{ required: true, message: "Please input your email!" }]}
           >
             <Input />
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
               Save Changes
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Modal
+        title="Enter your password"
+        visible={isDeleteModalVisible}
+        onCancel={handleDeletCancel}
+        footer={null}
+      >
+        <Form layout="vertical" onFinish={handleDeleteUser}>
+          <Form.Item
+            name="password"
+            label="password"
+            rules={[{ required: true, message: "Please enter your password" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" danger>
+              Delete Account
             </Button>
           </Form.Item>
         </Form>
